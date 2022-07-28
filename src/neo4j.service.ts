26ed9f429b66a3e46653ac34f68d73ad24500bd7
Atamer Atalay <atamer.atalay@signumtte.com>
2022-07-28 10:01:06 +0300

    .

--- src/neo4j.service.ts
+++ src/neo4j.service.ts
@@ -1154,38 +1154,7 @@ export class Neo4jService implements OnApplicationShutdown {
       }
     }
   }
-  //PARENT_OF
-  async addChildrenRelationById(child_id: string, parent_id: string) {
-    try {
-      if (!child_id || !parent_id) {
-        throw new HttpException(
-          add_parent_relation_by_id__must_entered_error,
-          400
-        );
-      }
-      const res = await this.write(
-        "MATCH (c {isDeleted: false}) where id(c)= $id MATCH (p {isDeleted: false}) where id(p)= $target_parent_id  MERGE (p)-[:PARENT_OF]-> (c)",
-        { id: parseInt(child_id), target_parent_id: parseInt(parent_id) }
-      );
-      const { relationshipsCreated } = res.summary.updateStatistics.updates();
-      if (relationshipsCreated === 0) {
-        throw new HttpException(
-          add_parent_relation_by_id__not_created_error,
-          400
-        );
-      }
-      return successResponse(res);
-    } catch (error) {
-      if (error.response?.code) {
-        throw new HttpException(
-          { message: error.response?.message, code: error.response?.code },
-          error.status
-        );
-      } else {
-        throw newError(error, "500");
-      }
-    }
-  }
+
   async deleteRelations(id: string) {
     const a = 1;
     try {
@@ -1216,8 +1185,6 @@ export class Neo4jService implements OnApplicationShutdown {
         throw new HttpException("id must entered", 400);
       }
       await this.addParentRelationById(_id, _target_parent_id);
-
-      await this.addChildrenRelationById(_id, _target_parent_id);
     } catch (error) {
       if (error.response?.code) {
         throw new HttpException(
